b725969b2977e6f2df81cc087e8935cad65559d4
yang <yang@getblueshift.com>
2022-10-07 12:52:08 -0700

    upgrade elixir/erlang version

--- lib/xandra/protocol/v5.ex
+++ lib/xandra/protocol/v5.ex
@@ -1,7 +1,7 @@
 defmodule Xandra.Protocol.V5 do
   @moduledoc false
 
-  use Bitwise
+  import Bitwise
 
   require Decimal
 
--- mix.exs
+++ mix.exs
@@ -11,7 +11,7 @@ defmodule Xandra.Mixfile do
     [
       app: :xandra,
       version: @version,
-      elixir: "~> 1.9",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
