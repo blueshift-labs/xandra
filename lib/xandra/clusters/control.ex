4cedb6a12b4c46d7c8b31d88a5e824028e81356a
yang <yang@getblueshift.com>
2023-02-23 14:52:50 -0800

    fix control kill

--- lib/xandra/clusters/control.ex
+++ lib/xandra/clusters/control.ex
@@ -53,7 +53,7 @@ defmodule Xandra.Clusters.Control do
         :start_link,
         [{cluster_name, host_id, address, rpc_address, port, data_center, options}]
       },
-      type: :worker
+      type: :supervisor
     }
   end
 
@@ -456,7 +456,7 @@ defmodule Xandra.Clusters.Control do
       [{pid, %{address: addr, rpc_address: rpc_addr}}] ->
         unless addr == rpc_addr do
           Logger.warn(
-            "Terminating discovery control connection for cluster [#{cluster_name}] at [#{addr}:#{port}]@[#{host_id}]"
+            "Terminating control connection for cluster [#{cluster_name}] at [#{addr}:#{port}]@[#{host_id}]"
           )
 
           DynamicSupervisor.terminate_child(Controls, pid)
